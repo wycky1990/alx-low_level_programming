ff76139532addbd9615e7f5d3a8e624af3e3366c
wycky1990 <wyclifffeomondi@gmail.com>
2024-06-20 23:42:50 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+ * main - starting point
+ *
+ * Return - 1 for fail 0 for success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
