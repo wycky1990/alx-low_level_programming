e05713daaee6b34384c561b2ba08d02739d6d314
wycky1990 <wyclifffeomondi@gmail.com>
2024-06-21 17:25:13 -0700

    size of variables

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ *  * main - A program that prints a line with puts function
+ *   * Return: 0 (success)
+ *   */
+int main(void)
+{
+	        puts("\"Programming is like building a multilingual puzzle");
+		        return (0);
+
+}i
