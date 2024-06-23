2219c5fab8443752ddb45938f177e122b55e275f
wycky1990 <wyclifffeomondi@gmail.com>
2024-06-22 20:19:01 -0700

    print updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	printf("Size of a char: %c byte(s)", sizeof(char));
 	printf("Size of a int: %u byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	printf("Size of a long long int: %llu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	printf("Size of a long int: %u byte(s)", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)", sizeof(long long int));
+	printf("Size of a float: %d byte(s)", sizeof(float));
 	return (0);
 
 }
