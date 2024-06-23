bfd859886b784550408fdfa821c2ed0ce6f4881f
wycky1990 <wyclifffeomondi@gmail.com>
2024-06-23 09:37:25 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+
+	return (0);
+}
