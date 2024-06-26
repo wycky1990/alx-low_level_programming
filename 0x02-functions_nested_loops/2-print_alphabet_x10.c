c4423121183aaa8d4bcd0714bca9c3ccd08558a6
wycky1990 <wyclifffeomondi@gmail.com>
2024-06-25 23:19:24 -0700

    print 10 lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 lines of alphabet
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+		i++;
+	}
+}
