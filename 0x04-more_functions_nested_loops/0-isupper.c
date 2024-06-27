a3030dffbdea358096a97a50d1c42124317d7378
wycky1990 <wyclifffeomondi@gmail.com>
2024-06-27 14:33:02 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a letter is upper
+ * @x: value to be checked
+ * Return: 1 for upper and 0 for alse
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
