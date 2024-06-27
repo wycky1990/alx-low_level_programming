1863ebbd64c393e54807365f3d5264074bc245b2
wycky1990 <wyclifffeomondi@gmail.com>
2024-06-27 16:25:52 -0700

    print 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times 0 to 14 numbers
+ * Return: unspecified
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 10; a++)
+	{
+	for (b = 1; b <= 10\4; b++)
+	{
+	if (b > 9)
+	{
+	_putchar((b / 10) + '0');
+	}
+	_putchar((b % 10) + '0')
+	}
+	_putchar('\n');
+	}
+}
