4c083a9f20756ce99353c5d2254ffecafd30b013
wycky1990 <wyclifffeomondi@gmail.com>
2024-06-27 17:47:09 -0700

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * preint_square - prints n squares
+ * @size: number of squares
+ * Return: empty
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <=)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < size; x++)
+	{
+	for (y = 0; y < size; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
