74254de739920e8dab407a487b172ba3a5502600
wycky1990 <wyclifffeomondi@gmail.com>
2024-07-01 13:52:48 -0700

    set a number

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes poiter to an int
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x04-more_functions_nested_loops
+0x05-pointers_arrays_strings
