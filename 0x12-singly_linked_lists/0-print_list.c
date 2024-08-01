4c7da7776eba057cf12b9356bb12103945164580
wycky1990 <wyclifffeomondi@gmail.com>
2024-08-01 15:35:08 -0700

    link lists assignment more explanation in comments

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
- * print_list - function that prints elements of a linked list
+ * print_list - function that prints all the elements of a linked list
  * @h: pointer to the list_t list to print
+ *
  * Return: the number of nodes printed
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t s = 0;
@@ -20,5 +20,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
+
 	return (s);
 }
