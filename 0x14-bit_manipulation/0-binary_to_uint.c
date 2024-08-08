d5540c4096f34c4159a88d0934d46e4dad3905bf
wycky1990 <wyclifffeomondi@gmail.com>
2024-08-08 15:59:26 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-        int a;
-        unsigned int c = 0;
+	int a;
+	unsigned int c = 0;
 
-        if (!b)
-                return (0);
-        for (a = 0; b[a]; a++)
-        {
-                if (b[a] < '0' || b[a] > '1')
-                        return (0);
-                c = 2 * c + (b[a] - '0');
-        }
-        return (c);
+	if (!b)
+		return (0);
+	for (a = 0; b[a]; a++)
+	{
+		if (b[a] < '0' || b[a] > '1')
+			return (0);
+		c = 2 * c + (b[a] - '0');
+	}
+	return (c);
 }
