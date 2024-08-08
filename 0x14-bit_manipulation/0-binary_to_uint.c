6b5540f8c2ee2924ff5acf18ba16bce83e1f6afe
wycky1990 <wyclifffeomondi@gmail.com>
2024-08-08 15:45:07 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,7 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
         int a;
-        unsigned int abraham = 0;
+        unsigned int c = 0;
 
         if (!b)
                 return (0);
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
         {
                 if (b[a] < '0' || b[a] > '1')
                         return (0);
-                abraham = 2 * abraham + (b[alx] - '0');
+                c = 2 * c + (b[a] - '0');
         }
-        return (abraham);
+        return (c);
 }
